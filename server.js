5682a1d23fab2ddb7f018893d9c03a53ce28ca53
anonymous <mranonymous2439@gmail.com>
2025-02-19 21:03:52 +0800

    added ping

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const { WebSocketServer } = require("ws");
 // Create WebSocket server
 const wss = new WebSocketServer({ port: 8081 });
 
+// Broadcast player count
 const broadcastPlayerCount = () => {
     const playerCount = wss.clients.size;
     wss.clients.forEach(client => {
@@ -14,6 +15,7 @@ const broadcastPlayerCount = () => {
 
 wss.on("connection", (ws) => {
     console.log("Client connected");
+    ws.isAlive = true; // Mark client as alive
     broadcastPlayerCount();
 
     ws.on("message", (message) => {
@@ -24,11 +26,28 @@ wss.on("connection", (ws) => {
         });
     });
 
+    ws.on("pong", () => {
+        ws.isAlive = true; // Client responded to ping
+    });
+
     ws.on("close", () => {
         console.log("Client disconnected");
         if (wss.clients.size > 0)
             setTimeout(() => broadcastPlayerCount(), 100);
     });
 });
+
+// Ping clients every 30 seconds
+setInterval(() => {
+    wss.clients.forEach(ws => {
+        if (!ws.isAlive) {
+            console.log("Terminating inactive client");
+            return ws.terminate();
+        }
+
+        ws.isAlive = false;
+        ws.ping(); // Send ping
+    });
+}, 30000); // Check every 30 seconds
 
 console.log("WebSocket server is running");
